3f2e4b8a45f229a574c26664d50eec4063af8796
Nico Bijl <nicobijl89@gmail.com>
2023-12-04 21:16:50 +0100

    Add day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,51 @@
 fun main() {
+    val numbers = mapOf(
+        "zero" to "z0ero",
+        "one" to "o1ne",
+        "two" to "t2wo",
+        "three" to "t3hree",
+        "four" to "f4our",
+        "five" to "f5ive",
+        "six" to "s6ix",
+        "seven" to "s7even",
+        "eight" to "e8ight",
+        "nine" to "n9ine"
+    )
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            (it.first { it.isDigit() }.toString() + it.last { it.isDigit() }.toString()).toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map { line: String ->
+                numbers.entries.fold(line){
+                    acc, (search, replace) -> acc.replace(search, replace)
+                }
+            }
+            .sumOf {
+                (it.first { it.isDigit() }.toString() + it.last { it.isDigit() }.toString()).toInt()
+            }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testResult = part1(testInput)
+
+    println(testResult)
+    check(testResult == 142)
+
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+
+
+    val testInput2 = readInput("Day01_test2")
+    val testResult2 = part2(testInput2)
+
+    println(testResult2)
+    check(testResult2 == 281)
+
+    println("part2: ${part2(input)}")
 }
