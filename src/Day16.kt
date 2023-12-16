0782031d4bb216641447f82459f65b3b6396eca0
nicobijl <n.bijl@portofrotterdam.com>
2023-12-16 15:31:52 +0100

    day 16 - part 2

--- src/Day16.kt
+++ src/Day16.kt
@@ -2,6 +2,9 @@ import Direction.DOWN
 import Direction.LEFT
 import Direction.RIGHT
 import Direction.UP
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import java.util.logging.Logger
 
 enum class Direction {
@@ -18,42 +21,38 @@ data class World(val grid: Array<CharArray>) {
             println("was already energized:  ${current.row} / ${current.column}")
             return;
         }
-        println("Now on pos: ${current.row} / ${current.column} char: $char direction: $direction (energized = ${countEnergized()})")
+//        println("Now on pos: ${current.row} / ${current.column} char: $char direction: $direction (energized = ${countEnergized()})")
 
         val next: List<Direction> = when (char) {
-            '/' ->
-                listOf(
-                    when (direction) {
-                        LEFT -> DOWN
-                        RIGHT -> UP
-                        UP -> RIGHT
-                        DOWN -> LEFT
-                    }
-                )
-
-            '\\' ->
-                listOf(
-                    when (direction) {
-                        LEFT -> UP
-                        RIGHT -> DOWN
-                        UP -> LEFT
-                        DOWN -> RIGHT
-                    }
-                )
-
-            '|' ->
+            '/' -> listOf(
                 when (direction) {
-                    LEFT -> listOf(UP, DOWN)
-                    RIGHT -> listOf(UP, DOWN)
-                    else -> listOf(direction)
+                    LEFT -> DOWN
+                    RIGHT -> UP
+                    UP -> RIGHT
+                    DOWN -> LEFT
                 }
+            )
 
-            '-' ->
+            '\\' -> listOf(
                 when (direction) {
-                    UP -> listOf(LEFT, RIGHT)
-                    DOWN -> listOf(LEFT, RIGHT)
-                    else -> listOf(direction)
+                    LEFT -> UP
+                    RIGHT -> DOWN
+                    UP -> LEFT
+                    DOWN -> RIGHT
                 }
+            )
+
+            '|' -> when (direction) {
+                LEFT -> listOf(UP, DOWN)
+                RIGHT -> listOf(UP, DOWN)
+                else -> listOf(direction)
+            }
+
+            '-' -> when (direction) {
+                UP -> listOf(LEFT, RIGHT)
+                DOWN -> listOf(LEFT, RIGHT)
+                else -> listOf(direction)
+            }
 
             else -> {
                 listOf(direction)
@@ -91,21 +90,37 @@ fun main() {
         return world.countEnergized()
     }
 
-    fun part2(fileContent: List<String>): Int {
+    fun part2(fileContent: Array<CharArray>): Int {
+        val startingPoints = (0..fileContent[0].count()).map { Vec2(0, it) }
+        runBlocking {
+            val mostEnergized = startingPoints.map {
+                async {
+                    val world = World(fileContent)
+                    world.move(it, DOWN)
+                    val energized = world.countEnergized()
+                    println("Start from $it , most energized = $energized")
+                    energized
+                }
+            }.awaitAll().max()
+            println("max items energized = $mostEnergized")
+        }
         return 0
     }
 
     val testInput = readArrays("Day16_sample")
     val input = readArrays("Day16")
-
-    val sampleResult = part1(testInput)
-    println("Test part 1: $sampleResult")
-    check(sampleResult == 46)
-    println("Result part 1: ${part1(input)}")
+//
+//    val sampleResult = part1(testInput)
+//    println("Test part 1: $sampleResult")
+//    check(sampleResult == 46)
+//
+//    val result1 = part1(input)
+//    println("Result part 1: $result1")
+//    check(result1 == 7415)
 
     // Part 2, updated getValue
-//    val testResult = part2(testInput)
-//    println("Test part 3: $testResult")
-//    check(testResult == 8)
-//    println("Result part 2: ${part2(input)}")
+    val testResult = part2(testInput)
+    println("Test part 2: $testResult")
+//    check(testResult == 51)
+    println("Result part 2: ${part2(input)}")
 }
